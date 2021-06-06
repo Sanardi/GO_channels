c0d653b44bb78bb67807ebf66e7f025ad8864764
sanardiazam <rubypromise@gmx.com>
2021-06-06 11:43:02 +0100

    messing about with okay syntax
    .. to understand if a channel is closed or the message is 0

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GO_channels
+
+basis Main function for reference when working with channels in go. 
+Covers Bi-directional as well as directed channels.
  
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
 func main() {
@@ -11,14 +10,20 @@ func main() {
 	ch := make(chan int, 1)
 
 	wg.Add(2)
-	go func(ch <-chan int, wg *sync.WaitGroup) {
-		fmt.Println(<-ch)
+	go func(ch chan int, wg *sync.WaitGroup) {
+		msg, ok := <-ch
+		fmt.Println(msg, ok)
+		//fmt.Println(<-ch)
+		//close(ch)
+		//fmt.Println(<-ch)
 		wg.Done()
 	}(ch, wg)
 	go func(ch chan<- int, wg *sync.WaitGroup) {
-		ch <- 42
-		time.Sleep(5 * time.Millisecond)
+		//ch <- 42
+		//time.Sleep(5 * time.Millisecond)
 		//fmt.Println(ch)
+		close(ch)
+		//ch <- 27
 		wg.Done()
 	}(ch, wg)
 
