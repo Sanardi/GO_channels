133d9c844214b194769ee144221f7bc8851fb0f2
sanardiazam <rubypromise@gmx.com>
2021-06-06 12:08:55 +0100

    Update main.go

--- main.go
+++ main.go
@@ -10,17 +10,22 @@ func main() {
 	ch := make(chan int, 1)
 
 	wg.Add(2)
-	go func(ch chan int, wg *sync.WaitGroup) {
-		if msg, ok := <-ch; ok {
-			fmt.Println(msg, ok)
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		//if msg, ok := <-ch; ok {
+		//fmt.Println(msg, ok)
+		for msg := range ch {
+			fmt.Println(msg)
 		}
+		wg.Done()
 		//fmt.Println(<-ch)
 		//close(ch)
 		//fmt.Println(<-ch)
-		wg.Done()
 	}(ch, wg)
 	go func(ch chan<- int, wg *sync.WaitGroup) {
-		ch <- 0
+		//ch <- 0
+		for i := 0; i < 10; i++ {
+			ch <- i
+		}
 		//time.Sleep(5 * time.Millisecond)
 		//fmt.Println(ch)
 		close(ch)
